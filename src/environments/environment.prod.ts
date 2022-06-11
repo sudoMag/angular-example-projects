8ca8bd3eec65f01b40d8463630f835b5278860a5
Manuel Casanova <m.casa2017@outlook.com>
2022-06-10 21:57:05 -0400

    resolve bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
-import 'dotenv/config';
 
 
 export const environment = {
   production: true,
-  NEWS_API_KEY: "pub_81661380b45580e2bd0cc55b99bb0ad9ef05",
+  NEWS_API_KEY: "pub_81661380b45580e2bd0cc55b99bb0ad9ef05", // I haven´t where save it 😭
 };
